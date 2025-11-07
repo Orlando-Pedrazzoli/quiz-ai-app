0575cfff94e9662bbde1c156822416d50e9f7c03
Orlando Pedrazzoli <pedrazzoliorlando@gmail.com>
2025-11-07 14:02:25 +0000

    update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  webpack: (config) => {
-    // Adiciona suporte para pdf-parse
-    config.resolve.alias.canvas = false;
-    config.resolve.alias.encoding = false;
-    return config;
-  }
-}
 
-module.exports = nextConfig
+  turbopack: {
+    resolveAlias: {
+      canvas: '',
+      encoding: '',
+    },
+  },
+};
+
+module.exports = nextConfig;
